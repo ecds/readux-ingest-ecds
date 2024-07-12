a1c53102b785dcfd61e671f6e2867a0b410ffb33
Jay Varner <jayvarner@gmail.com>
2024-07-12 13:27:02 -0400

    Add form html for bulk

--- readux_ingest_ecds/forms.py
+++ readux_ingest_ecds/forms.py
@@ -6,20 +6,6 @@ class MultipleFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
 
 
-class MultipleFileField(forms.FileField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
-
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = [single_file_clean(data, initial)]
-        return result
-
-
 class BulkVolumeUploadForm(forms.ModelForm):
     class Meta:
         model = Bulk
